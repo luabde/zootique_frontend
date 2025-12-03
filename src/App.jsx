10a88c03e873030733eda5854d23cdede3cbd352
Lucía Abellán <luciaabellan6@gmail.com>
2025-12-03 09:19:59 +0100

    Arreglo warning fotos

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
     <div className='flex justify-between flex-col lg:flex-row max-w-7xl mx-auto lg:p-4 md:p-3 p-1'>
       <div className='flex gap-3 items-center justify-center'>
-        <img className='w-16' src="../public/img/logo.png" alt="Logo zootique" />
+        <img className='w-16' src="/img/logo.png" alt="Logo zootique" />
         <span className='font-medium'>Zootique</span>
       </div>
       <nav className='flex gap-4 justify-center items-center'>
